6091f26de082d94e35ff4731bb1b6cd42cf68f1f
jpgfour <jeremy.grise@gmail.com>
2020-07-07 11:22:11 -0500

    working on part 2 - got persistence into the project

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @Size(min = 1, max = 50)
+    @NotBlank
+    private String location;
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    //no-arg constructor for hibernate
+    public Employer() {}
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(min = 1, max = 100)
+    @NotBlank
+    private String verboseSkill;
+
+    public String getVerboseSkill() {
+        return verboseSkill;
+    }
+
+    public void setVerboseSkill(String verboseSkill) {
+        this.verboseSkill = verboseSkill;
+    }
+
+    //no-arg constructor for hibernate
+    public Skill() {}
 }
